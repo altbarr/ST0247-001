e2cdc7bb8560abe6dabf948092d2bf2b910dde76
altbarr <98355034+altbarr@users.noreply.github.com>
2022-01-26 00:11:50 -0500

    Update GraphAM.py

--- talleres/taller01/GraphAM.py
+++ talleres/taller01/GraphAM.py
@@ -1,17 +1,22 @@
+import numpy as np
+from collections import deque
+
 class GraphAm:
-
     def __init__(self, size):
-
-
-    def getEdges(self):
-
+      dimensiones = (size,size)#hola
+      self.matriz = np.zeros( dimensiones )    
 
     def getWeight(self, source, destination):
-
+      return self.matriz[source][destination]
 
     def addArc(self, source, destination, weight):
+    	self.matriz[source][destination] = weight
 
     def getSuccessors(self, vertex):
-
-    def __str__(self):
-
+      filaVertice = self.matriz[vertex]
+      respuesta = []
+      for j in range(0
+      ,size):
+        if filaVertice[j] != 0:
+           respuesta.append(j)
+      return respuesta
