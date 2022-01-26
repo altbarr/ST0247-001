c616c67db15a55c1aaefbc1a23700234b9e51f4b
pedrocardenas19 <83480607+pedrocardenas19@users.noreply.github.com>
2022-01-26 00:40:18 -0500

    Update GraphAL.py

--- talleres/taller01/GraphAL.py
+++ talleres/taller01/GraphAL.py
@@ -1,22 +1,21 @@
-import numpy as np
 from collections import deque		
 class GraphAL:
-    def __init__(self, size):
-      self.arregloDeListas = [0]*size
-      for i in range(0,size):
-         self.arregloDeListas[i] = deque()
+  def __init__(self, size):
+    self.arregloDeListas = [0]*size
+    for i in range(0,size):
+      self.arregloDeListas[i] = deque()
 
-    def addArc(self, vertex, destination, weight):
-         fila = self.arregloDeListas[vertex]
-         parejaDestinoPeso = (destination, weight)
-         fila.append(parejaDestinoPeso)
+  def addArc(self, vertex, destination, weight):
+    fila = self.arregloDeListas[vertex]
+    parejaDestinoPeso = (destination, weight)
+    fila.append(parejaDestinoPeso)
 
-    def getSuccessors(self, vertice):
-    		for i in range(0,vertice):
-       		successors = self.arregloDeListas
-					return successors
+  def getSuccessors(self, vertice):
+    for i in range(0,vertice):
+      successors = self.arregloDeListas
+    return successors
 
-    def getWeight(self, source, destination):
-    		for i in range(0,destination):
-        	weight = self.arregloDeListas
-					return weight
+  def getWeight(self, source, destination):
+    for i in range(0,destination):
+      weight = self.arregloDeListas
+    return weight
